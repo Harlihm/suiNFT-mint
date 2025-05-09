354652d164120c412a6291235dbf6aa2c76bd3f0
harlihm <harlihm2@gmail.com>
2025-05-10 00:43:58 +0100

    button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <>
+    <div><h1>here </h1></div>
      <header>
         <ConnectButton/>
       </header>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import {WalletProvider} from '@suiet/wallet-kit';
 import '@suiet/wallet-kit/style.css';
+import ReactDOM from 'react-dom/client';
+import App from "./App"
 
 // take react@18 project as an example
 ReactDOM.createRoot(document.getElementById('root')).render(
